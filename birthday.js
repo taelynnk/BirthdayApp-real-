f0594f6b0eb4b1e091a674e4324447955d02562a
Lizet N Reyes Rodas <lr40247@srcs.loc>
2018-02-09 11:10:56 -0800

    countdown function js

---
+++ birthday.js
@@ -0,0 +1,12 @@
+function birthdayCountDown(date) {
+    var today = new Date();
+    var myBirthday = new Date(date);
+    myBirthday.setFullYear(today.getFullYear());
+    if (today.getTime() > myBirthday.getTime()) {
+        myBirthday.setFullYear(today.getFullYear() + 1);
+    }
+    var diff = myBirthday.getTime() - today.getTime();
+    var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+    return ("There are " + days+ " days until your birthday");
+}
