4681ade1450260463a732f981212000a6b337fbd
Taelynn A Knutson <tk62539@srcs.loc>
2018-02-13 12:15:07 -0800

    finish calculate age function

--- birthday.js
+++ birthday.js
@@ -1,3 +1,5 @@
+var myButton = document.getElementById('calculateAge');
+
 function birthdayCountDown(date) {
     var today = new Date();
     var myBirthday = new Date(date);
@@ -8,10 +10,23 @@ function birthdayCountDown(date) {
     var diff = myBirthday.getTime() - today.getTime();
     var days = Math.floor(diff / (1000 * 60 * 60 * 24));
 
-    return ("There are " + days+ " days until your birthday");
+    return ("There are " + days + " days until your birthday");
 }
 
 function incorrectBirthDate(date) {
-    var birthInput = new Date(mm/dd/yyyy);
-
+    var birthInput = new Date(mm / dd / yyyy);
 }
+
+function calculateAge(inputBirthday) {
+    var today = new Date();
+    var birthday = new Date(inputBirthday);
+    return Math.floor((new Date() - birthday) / 1000 / 60 / 60 / 24 / 365);
+}
+
+function displayCalculatedAge() {
+    var input = document.getElementById('inputBirthday').value;
+    var age = calculateAge(input);
+    document.getElementById('age').textContent = "You are " + age + " years of age.";
+}
+
+myButton.addEventListener('click', displayCalculatedAge);
