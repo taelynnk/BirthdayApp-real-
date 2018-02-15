36d061009428b7f1eed3121bbbc81c61875e8719
Daniel A Ramirez <dr82913@srcs.loc>
2018-02-15 11:55:50 -0800

    Fixed incorrectBirthDate

--- birthday.js
+++ birthday.js
@@ -13,9 +13,8 @@ function birthdayCountDown(date) {
     return ("There are " + days + " days until your birthday");
 }
 
-function incorrectBirthDate(date) {
+function incorrectBirthDate(inputBirthday) {
     var birthInput = new Date(mm / dd / yyyy);
-    var birthInput = new Date(date);
 if (birthInput !== date) {
     return "Sorry, but you did not enter a date in the correct format.";
 } else {
