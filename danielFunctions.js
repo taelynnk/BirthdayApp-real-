a583875300a8a6ff6a0476eb9b4761324a218c76
Daniel A Ramirez <dr82913@srcs.loc>
2018-02-26 11:41:51 -0800

    Added back my js file

---
+++ danielFunctions.js
@@ -0,0 +1,11 @@
+function incorrectBirthDate(inputBirthday) {
+    var birthInput = new Date(mm/dd/yyyy);
+    var mm = "123456789101112"
+    var dd = "123456789101112"
+    var yyyy = "123456789101112"
+if (birthInput !== date) {
+    return "Sorry, but you did not enter a date in the correct format.";
+} else {
+    return myBirthday;
+  }
+}
