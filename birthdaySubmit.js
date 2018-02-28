bdc4c5617dd55f1b827d73b7bd2b602affec8213
Daniel A Ramirez <dr82913@srcs.loc>
2018-02-28 12:22:08 -0800

    Added User Story 4 function to main js file

--- birthdaySubmit.js
+++ birthdaySubmit.js
@@ -79,3 +79,13 @@ function findZodiac(inputZodiac) {
     }
 
     button.addEventListener('click', displayfindZodiac);
+
+function BirthDateCheck(inputNum) {
+    var birthInput = mm/dd/yyyy;
+    var d = new Date(birthInput);
+if (birthInput.length < 10) {
+    f(isNaN(d.getTime())) {
+    } else {
+        return "That isn't the correct format for a birth date.";
+    }
+}
