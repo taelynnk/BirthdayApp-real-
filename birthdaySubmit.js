a9d78c0173a3420f23ee43ced52f4b8b23968ac8
Lizet N Reyes Rodas <lr40247@srcs.loc>
2018-03-02 10:38:51 -0800

    new look

--- birthdaySubmit.js
+++ birthdaySubmit.js
@@ -12,28 +12,6 @@ function displayCalculatedAge() {
 }
 myButton.addEventListener('click', displayCalculatedAge);
 
-var button = document.getElementById('countdownbutton');
-
-function countdownbutton(inputdate) {
-    var today = new Date();
-    var myBirthday = new Date(inputdate) ;
-    myBirthday.setFullYear(today.getFullYear());
-    if (today.getTime() > myBirthday.getTime()) {
-        myBirthday.setFullYear(today.getFullYear() );
-    }
-    var diff = myBirthday.getTime() - today.getTime();
-    var days = Math.floor(diff / (1000 * 60 * 60 * 24)) + 1;
-
-    return (days);
-}
-
-function displaycountdownbutton() {
-    var input = document.getElementById('inputdate').value;
-    var countdown = countdownbutton(input);
-    return document.getElementById('countdown').textContent = countdown + " day(s) until your next birthday!";
-}
-
-button.addEventListener('click', displaycountdownbutton);
 
 var button2 = document.getElementById('findZodiac');
 
@@ -78,4 +56,27 @@ function findZodiac(inputZodiac) {
 
     }
 
-    button.addEventListener('click', displayfindZodiac);
+    button2.addEventListener('click', displayfindZodiac);
+
+var button = document.getElementById('countdownbutton');
+
+function countdownbutton(inputdate) {
+    var today = new Date();
+    var myBirthday = new Date(inputdate) ;
+    myBirthday.setFullYear(today.getFullYear());
+    if (today.getTime() > myBirthday.getTime()) {
+        myBirthday.setFullYear(today.getFullYear() );
+    }
+    var diff = myBirthday.getTime() - today.getTime();
+    var days = Math.floor(diff / (1000 * 60 * 60 * 24)) + 1;
+
+    return (days);
+}
+
+function displaycountdownbutton() {
+    var input = document.getElementById('inputdate').value;
+    var countdown = countdownbutton(input);
+    return document.getElementById('countdown').textContent = countdown + " day(s) until your next birthday!";
+}
+
+button.addEventListener('click', displaycountdownbutton);
